f5b4170f6291d7555770003719707d41e2f7f390
ryo <ryo@RyoK.local>
2021-01-14 22:02:10 +0900

    first commit

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Koke";
+		System.out.println(name);
+		name = "nekoneko";
+		System.out.println(name);
+		int LuckyNum = 2021;
+		System.out.println(LuckyNum);
+	}
+}
